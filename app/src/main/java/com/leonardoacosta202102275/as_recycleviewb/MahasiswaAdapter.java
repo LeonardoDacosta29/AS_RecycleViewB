f6b8d8bbc584feae93b6d4e12929165b20820598
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2023-06-12 10:42:04 +0700

    mainActivity
    error nahh

--- app/src/main/java/com/leonardoacosta202102275/as_recycleviewb/MahasiswaAdapter.java
+++ app/src/main/java/com/leonardoacosta202102275/as_recycleviewb/MahasiswaAdapter.java
@@ -1,11 +1,14 @@
 package com.leonardoacosta202102275.as_recycleviewb;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Locale;
 
 public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaViewHolder>
 {
@@ -20,16 +23,41 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaViewHolder>
     @Override
     public MahasiswaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.list_mahasiswa,parent,false);
+        return new MahasiswaViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MahasiswaViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MahasiswaViewHolder holder, int position)
+    {
+        MahasiswaModel mm = _mahasiswaModelList.get(position);
+
+        holder._jkImageView.setImageResource(R.drawable.boy);
+
+        if (mm.getJenisKelamin().toLowerCase().equals("Perempuan"))
+        {
+            holder._jkImageView.setImageResource(R.drawable.girl);
+        }
+
+        holder._nimTextView.setText(mm.getNIM());
+        holder._namaTextView.setText(mm.getNama());
+        holder._jkTextView.setText(mm.getJenisKelamin());
+
+        String jp = mm.getJP();
+        jp = jp.substring(0, 2);//hanya ambil 2 karakter depan
+        holder._jpTextView.setText(jp);
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount()
+    {
+        int count = 0;
+        if (_mahasiswaModelList != null)
+        {
+            count = _mahasiswaModelList.size();
+        }
+        return count;
     }
 }
--- app/src/main/java/com/leonardoacosta202102275/as_recycleviewb/MainActivity.java
+++ app/src/main/java/com/leonardoacosta202102275/as_recycleviewb/MainActivity.java
@@ -1,14 +1,47 @@
 package com.leonardoacosta202102275.as_recycleviewb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.HeaderViewListAdapter;
+import android.widget.Toast;
+
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView _recyclerView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
+        AsyncHttpClient ahc = new AsyncHttpClient();
+        String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
+
+        ahc.get(url, new AsyncHttpResponseHandler()
+        {
+            @Override
+            public void onSucces(int statusCode, Header[] headers, byte[] responseBody)
+            {
+                Gson g = new Gson();
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType);
+
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+                _recyclerView1.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView1.setAdapter(ma);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responBody, Throwable error)
+            {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
